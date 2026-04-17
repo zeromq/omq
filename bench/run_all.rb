0ef015d2c4d83bfc111236859a7579aba8186666
Patrik Wenger <paddor@gmail.com>
2026-04-17 14:34:44 +0200

    cut bloat from bench suite

--- bench/run_all.rb
+++ bench/run_all.rb
@@ -4,6 +4,6 @@
 
 ENV["OMQ_BENCH_RUN_ID"] = Time.now.strftime("%Y-%m-%dT%H:%M:%S")
 
-%w[push_pull pub_sub req_rep router_dealer dealer_dealer pair].each do |pattern|
+%w[push_pull pub_sub req_rep router_dealer pair].each do |pattern|
   system("ruby", "--yjit", File.join(__dir__, pattern, "omq.rb")) || abort("#{pattern} failed")
 end
